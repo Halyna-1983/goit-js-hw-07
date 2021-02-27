1a6d13636fcc4f7ff304039ad4827a3211c0ab51
Halyna-1983 <72556196+Halyna-1983@users.noreply.github.com>
2021-02-27 23:16:14 +0200

    tast - 5 edition

--- task-5.js
+++ task-5.js
@@ -17,6 +17,14 @@ function onInputChange(event) {
     }  
 }
 
+//************************************************************* */
 
+// const inputRef = document.querySelector('#name-input');
+// const outputRef = document.querySelector('#name-output');
+// const changedText = (event) => {
+//   if (event.target.value.length > 0) {
+//     outputRef.textContent = event.target.value;
+//   } else outputRef.textContent = "незнакомец";
+// };
 
-    
+// inputRef.addEventListener("input", changedText);
